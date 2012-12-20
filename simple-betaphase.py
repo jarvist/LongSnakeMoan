ee12c33fd658b4c798b2ae521911024804aeb147
Jarvist Moore Frost <jarvist@fakemail.fake>
2012-12-20 15:59:41 +0000

    Update before I start fiddling with the main file to get better statistics.

--- simple-betaphase.py
+++ simple-betaphase.py
@@ -20,10 +20,12 @@ pl.ion()
 # SiteE = Ionisation Potential
 # J     = electron hopping integral
 siteE=5.853060  #5.853060
-J=0.6
+J=0.6 #Maximal hopping integral (inter-torsional theta=0)
 trapE=0.176
 siteSigma=0.05 #0.05
 JSigma=0.5 #0.5
+ThetaMin=pi/4 #Pi/4
+nsites=100
 
 # specify model
 # Define Lattice Vectors
@@ -43,18 +45,20 @@ my_model.display()
 # solve model
 # generate k-points
 path=[[-0.5],[0.5]]
-kpts=k_path(path,100)
+kpts=k_path(path,nsites)
 #print "kpts, ",kpts
 
 (evals,eigs)=my_model.solve_all(kpts,eig_vectors=True)
 
-finite_model=my_model.cut_piece(100,0,glue_edgs=False) #glue_edgs makes system periodic
+print "Infinite model:\n",evals
+
+finite_model=my_model.cut_piece(nsites,0,glue_edgs=False) #glue_edgs makes system periodic
 
 print finite_model._hoppings
 
-for i in range(99):
+for i in range(nsites-1):
     finite_model._site_energies[i]=finite_model._site_energies[i]+random.gauss(0,siteSigma)
-    finite_model._hoppings[i][0]=finite_model._hoppings[i][0]*(cos(random.gauss(0,JSigma))**2)
+    finite_model._hoppings[i][0]=finite_model._hoppings[i][0]*(cos(random.gauss(ThetaMin,JSigma))**2) #random around 45 degrees
 
 finite_model.display()
 
@@ -73,8 +77,8 @@ pl.title("Beta-PFO 1D pyTB - Disordered\nSigma=%s JSigma=%s"%(siteSigma,JSigma))
 
 energies=[]
 
-for i in range(75,85,1):
-    finite_model._site_energies[i]=finite_model._site_energies[i]-trapE
+#for i in range(75,85,1):
+#    finite_model._site_energies[i]=finite_model._site_energies[i]-trapE
 
 F=0.01 #V/site, roughly equiv. to V/nm. 
 #Thomas K says 1V/100nm is about right for an Organic Solar Cell at Short Circuit
@@ -85,8 +89,8 @@ F=0.01 #V/site, roughly equiv. to V/nm.
 
 colours='bgrcmykkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk'
   # Aaah, we fade to grey (fade to grey)
-b=10
-for i, colour in zip(range(25,45,b),colours):
+b=50
+for i, colour in zip(range(0,45,b),colours):
     print "Iterate value i=",i," colour value",colour
     (evals,eigs)=finite_model.solve_all(eig_vectors=True)
     print evals[0]
@@ -94,15 +98,15 @@ for i, colour in zip(range(25,45,b),colours):
 
     #Plot Eigenvalues with filled-in Eigenvectors^2 / electron probabilities
     pl.subplot(411)
-    for j in range(0,5):
+    for j in range(0,5): #Number of eigenvalues plotted (electron wavefns)
         pl.fill_between(range(100),evals[j],evals[j]+eigs[j]*eigs[j], facecolor=colour)
     pl.ylabel("Occ %")
-    pl.ylim((3.8,5.0))
+    #pl.ylim((3.8,5.0))
     pl.yticks(fontsize=9)
 
     #Plot Hamiltonian
     pl.subplot(412)
-    pl.ylim((5.5,6.0))
+    #pl.ylim((5.5,6.0))
     pl.ylabel("Hamiltonian\n(eV)")
     pl.yticks(fontsize=9)
 
@@ -110,7 +114,7 @@ for i, colour in zip(range(25,45,b),colours):
 
     #Plot Eigenvalues - not sure how useful this display is
     pl.subplot(413)
-    pl.ylim((0.0,1.0))
+   # pl.ylim((0.0,1.0))
     pl.yticks(fontsize=9)
 
     pl.ylabel("Js (eV)")
@@ -125,15 +129,17 @@ for i, colour in zip(range(25,45,b),colours):
     pl.yticks(fontsize=9)
     pl.xlabel("Tight Binding Site (#) / DoS Histogram (eV)") #xLabel for shared axes
 
+    # Histogram of TB Eigenvalues (i.e. DoS)
     pl.hist(evals,50,color=colour)
-    pl.hist(zip(*finite_model._hoppings)[0],50,color=colour)
+    # Histogram of Hopping Integrals (funky zip command to rearrange J magnitude into 1D vector)
+#    pl.hist(zip(*finite_model._hoppings)[0],50,color=colour)
 
     pl.draw()
 
     # Modifies the 'Beta Bubble' region of the Hamiltonian
     for j in range(i,i+b): #filthy
 #        finite_model._site_energies[j]=finite_model._site_energies[j]-trapE #From NWCHEM / BNL calc on PFO
-        finite_model._hoppings[j][0]=J +0.3 #Perfect transfer integrals
+        finite_model._hoppings[j][0]=J #Perfect transfer integrals
     #    finite_model._hoppings[j][0]=finite_model._hoppings[j][0]-0.3 #i.e. in addition to any disorder
     # See: /work/jmf02/NWCHEM/BETA-PHASE/tune_beta_alpha
     #finite_model._site_energies[10]=finite_model._site_energies[10]-0.01
@@ -147,7 +153,7 @@ for i, colour in zip(range(25,45,b),colours):
 
 #For some reason this finally holds the window open
 
-
+print "Lowest Eigenvalues:\n", energies
 
 print "Saving figures...(one moment please)"
 now=datetime.datetime.now().strftime("%Y-%m-%d-%H:%M")
